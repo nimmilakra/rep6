0a444a2ca5ec4254797e6c1021db5f3cd2bdee00
nimmilakra <nimmilakra566@gmail.com>
2018-02-11 07:11:01 -0800

    hi

---
+++ demoExample/src/main/java/com/demoExample/App.java
@@ -0,0 +1,13 @@
+package com.demoExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demoExample/src/test/java/demolesson/Demo1.java
@@ -0,0 +1,19 @@
+package demolesson;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("Hello World");
+	}
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Hello Darling");
+	}
+
+}
